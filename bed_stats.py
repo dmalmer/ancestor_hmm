6d16ef86e3dac382439197c84fadbe39a6b0b5f5
Daniel Malmer <dmalmer@gmail.com>
2014-06-30 11:13:59 -0600

    add basic overall stats script

---
+++ bed_stats.py
@@ -0,0 +1,53 @@
+
+#run with:
+# python bed_stats.py ISS unique
+# python bed_stats.py ISS full
+# python bed_stats.py ILS unique
+# python bed_stats.py ILS full
+
+import sys
+
+if __name__ == "__main__":
+    strain = sys.argv[1]
+
+    if sys.argv[2].lower() == 'full':
+        snp_type = 'Full'
+    elif sys.argv[2].lower() == 'unique':
+        snp_type = 'temporary'
+
+    total = 0
+
+    print 'Classifications per chromosome:'
+
+    #chr 1 through 19
+    for i in range(1,20):
+        cur_chr = 'chr'+str(i)
+
+        f_in = open('results/' + strain + '_' + snp_type + '_sorted_' + cur_chr + '_hmm-out.txt','r')
+
+        lines = f_in.readlines()
+        print cur_chr + ': ' + str(len(lines))
+
+        total += len(lines)
+
+    #chrX
+    cur_chr = 'chrX'
+
+    f_in = open('results/' + strain + '_' + snp_type + '_sorted_' + cur_chr + '_hmm-out.txt','r')
+
+    lines = f_in.readlines()
+    print cur_chr + ': ' + str(len(lines))
+
+    total += len(lines)
+
+    #chrY
+    cur_chr = 'chrY'
+
+    f_in = open('results/' + strain + '_' + snp_type + '_sorted_' + cur_chr + '_hmm-out.txt','r')
+
+    lines = f_in.readlines()
+    print cur_chr + ': ' + str(len(lines))
+
+    total += len(lines)
+
+    print '\nTotal classifications: ' + str(total)

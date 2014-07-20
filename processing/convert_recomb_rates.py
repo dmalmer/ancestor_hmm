950a1b9a6aa379577ee45f86fb6125f3e6b1810b
Daniel Malmer <dmalmer@gmail.com>
2014-07-20 16:02:59 -0600

    fixed indents

--- processing/convert_recomb_rates.py
+++ processing/convert_recomb_rates.py
@@ -22,24 +22,24 @@ normal_num = 1000/(rates_max - rates_min)
 #  --anything > 2 std_dev's from mean, change to max (1000)
 outside_ct = 0
 with open('../data/mouse_recomb_rates.wig', 'w') as f_out:
-	f_out.write('track type=wiggle_0 graphType=heatmap\n')
+    f_out.write('track type=wiggle_0 graphType=heatmap\n')
 
-	with open('../data/mouse_recomb_rates.csv', 'r') as f_in:
-		next(f_in)
-		chrom = ''
+    with open('../data/mouse_recomb_rates.csv', 'r') as f_in:
+        next(f_in)
+        chrom = ''
 
-		for line in f_in:
-			splits = line.split(',')
+        for line in f_in:
+            splits = line.split(',')
 
-			new_chrom = splits[0]
-			if new_chrom != chrom:
-				chrom = new_chrom
-				f_out.write('variableStep chrom=%s\n' % chrom)
+            new_chrom = splits[0]
+            if new_chrom != chrom:
+                chrom = new_chrom
+                f_out.write('variableStep chrom=%s\n' % chrom)
 
-			if float(splits[2]) > rates_mean + 2*rates_std_dev:
-				f_out.write('%i\t%i\n' % (int(float(splits[1])*1000), 1000))
-				outside_ct += 1
-			else:
-				f_out.write('%i\t%i\n' % (int(float(splits[1])*1000), int(float(splits[2]) * normal_num)))
+            if float(splits[2]) > rates_mean + 2*rates_std_dev:
+                f_out.write('%i\t%i\n' % (int(float(splits[1])*1000), 1000))
+                outside_ct += 1
+            else:
+                f_out.write('%i\t%i\n' % (int(float(splits[1])*1000), int(float(splits[2]) * normal_num)))
 
 print outside_ct

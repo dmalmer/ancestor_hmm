93d8f1c22ce6e839998fda9a1235182376cedb0e
Daniel Malmer <dmalmer@gmail.com>
2014-08-08 11:45:22 -0700

    Add recomb rates and parallelism to qsub run script

--- run_hmm.sh
+++ run_hmm.sh
@@ -4,9 +4,9 @@
 
 #PBS -q long
 #PBS -l nodes=1:ppn=1
-#PBS -l mem=256mb
+#PBS -l mem=1gb
 
 #PBS -m ae
 #PBS -M daniel.malmer@colorado.edu
 
-python2.7 /Users/dama9282/AncestorInference/hmm/hmm_main.py /Users/dama9282/AncestorInference/data/${STRAIN}_temporary_sorted${CHR}.bed
+python2.7 /Users/dama9282/AncestorInference/hmm/hmm_main.py -i /Users/dama9282/AncestorInference/data/${STRAIN}_temporary_sorted${CHR}.bed -r -p

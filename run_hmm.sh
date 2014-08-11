14d52f77f64da684826548a7f7b03723f0acd28c
Daniel Malmer <dmalmer@gmail.com>
2014-08-11 10:55:05 -0700

    Remove ppn setting

--- run_hmm.sh
+++ run_hmm.sh
@@ -3,10 +3,10 @@
 #PBS -V
 
 #PBS -q long
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=1
 #PBS -l mem=1gb
 
 #PBS -m ae
 #PBS -M daniel.malmer@colorado.edu
 
-python2.7 /Users/dama9282/AncestorInference/hmm/hmm_main.py -i /Users/dama9282/AncestorInference/data/${STRAIN}_temporary_sorted${CHR}.bed -r -p
+python2.7 /Users/dama9282/AncestorInference/hmm/hmm_main.py -i /Users/dama9282/AncestorInference/data/${STRAIN}_temporary_sorted${CHR}.bed -r -p -v

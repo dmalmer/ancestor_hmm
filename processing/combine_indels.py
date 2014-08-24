5ce1337e7fb85de528d20eb13126d6e92fba50e4
Daniel Malmer <dmalmer@gmail.com>
2014-08-24 17:36:38 -0600

    Split ancestor indels to sep ins and del files

--- processing/combine_indels.py
+++ processing/combine_indels.py
@@ -2,14 +2,15 @@
 from collections import defaultdict
 from operator import itemgetter
 
-indels_by_chr = defaultdict(lambda : defaultdict(str))
+insertions_by_chr = defaultdict(lambda : defaultdict(str))
+deletions_by_chr = defaultdict(lambda : defaultdict(str))
 
 for s in ('A', 'ARK', 'BALBc', 'C3HHe', 'DBA2'):
     with open('data/' + s + '_insertions.bed', 'r') as f:
         line = f.readline().strip()
         while line != '':
             cols = line.split('\t')
-            indels_by_chr[cols[0]][(int(cols[1]), int(cols[2]))] += s + '_'
+            insertions_by_chr[cols[0]][(int(cols[1]), int(cols[2]))] += s + '_'
 
             line = f.readline().strip()
 
@@ -17,13 +18,20 @@ for s in ('A', 'ARK', 'BALBc', 'C3HHe', 'DBA2'):
         line = f.readline().strip()
         while line != '':
             cols = line.split('\t')
-            indels_by_chr[cols[0]][(int(cols[1]), int(cols[2]))] += s + '_'
+            deletions_by_chr[cols[0]][(int(cols[1]), int(cols[2]))] += s + '_'
 
             line = f.readline().strip()
 
-with open('data/ancestor_indels.bed', 'w') as f:
+with open('data/ancestor_insertions.bed', 'w') as f:
     for curr_chr in ('chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12',
                      'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chrX', 'chrY'):
-        sorted_dict = sorted(indels_by_chr[curr_chr].iteritems(), key=itemgetter(0))
+        sorted_dict = sorted(insertions_by_chr[curr_chr].iteritems(), key=itemgetter(0))
+        for item in sorted_dict:
+            f.write(curr_chr + '\t' + str(item[0][0]) + '\t' + str(item[0][1]) + '\t' + str(item[1][:-1]) + '\n')
+
+with open('data/ancestor_deletions.bed', 'w') as f:
+    for curr_chr in ('chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12',
+                     'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chrX', 'chrY'):
+        sorted_dict = sorted(deletions_by_chr[curr_chr].iteritems(), key=itemgetter(0))
         for item in sorted_dict:
             f.write(curr_chr + '\t' + str(item[0][0]) + '\t' + str(item[0][1]) + '\t' + str(item[1][:-1]) + '\n')

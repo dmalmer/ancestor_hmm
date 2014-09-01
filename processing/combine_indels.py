d2829372eda5d935aca14322504636e21faba8fe
Daniel Malmer <dmalmer@gmail.com>
2014-08-31 22:03:01 -0600

    Updated to merge overlapped regions

--- processing/combine_indels.py
+++ processing/combine_indels.py
@@ -2,10 +2,16 @@
 from collections import defaultdict
 from operator import itemgetter
 
+
+def clean_label(label):
+    return '_'.join(sorted(list(set(label[:-1].split('_')))))
+
+
 insertions_by_chr = defaultdict(lambda : defaultdict(str))
 deletions_by_chr = defaultdict(lambda : defaultdict(str))
 
-for s in ('A', 'ARK', 'BALBc', 'C3HHe', 'DBA2'):
+# read in anc indels
+for s in ('A', 'AKR', 'BALBc', 'C3HHe', 'DBA2'):
     with open('data/' + s + '_insertions.bed', 'r') as f:
         line = f.readline().strip()
         while line != '':
@@ -22,16 +28,47 @@ for s in ('A', 'ARK', 'BALBc', 'C3HHe', 'DBA2'):
 
             line = f.readline().strip()
 
+# write to new file, merging overlaps
 with open('data/ancestor_insertions.bed', 'w') as f:
     for curr_chr in ('chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12',
                      'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chrX', 'chrY'):
         sorted_dict = sorted(insertions_by_chr[curr_chr].iteritems(), key=itemgetter(0))
-        for item in sorted_dict:
-            f.write(curr_chr + '\t' + str(item[0][0]) + '\t' + str(item[0][1]) + '\t' + str(item[1][:-1]) + '\n')
+        i = 0
+        while i < len(sorted_dict):
+            pos_start = sorted_dict[i][0][0]
+            pos_end = sorted_dict[i][0][1]
+            label = sorted_dict[i][1]
+            i += 1
+            #print 'start: s = ' + str(pos_start) + ', e = ' + str(pos_end) + ', l = ' + label
+            while i < len(sorted_dict) and sorted_dict[i][0][0] < pos_end:
+                #print 'found: ' + str(sorted_dict[i])
+                pos_start = min(sorted_dict[i][0][0], pos_start)
+                pos_end = max(sorted_dict[i][0][1], pos_end)
+                label += sorted_dict[i][1]
+                i += 1
+            #print 'final: s = ' + str(pos_start) + ', e = ' + str(pos_end) + ', l = ' + clean_label(label)
+            f.write(curr_chr + '\t' + str(pos_start) + '\t' + str(pos_end) + '\t' + clean_label(label) + '\n')
 
 with open('data/ancestor_deletions.bed', 'w') as f:
     for curr_chr in ('chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12',
                      'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chrX', 'chrY'):
         sorted_dict = sorted(deletions_by_chr[curr_chr].iteritems(), key=itemgetter(0))
-        for item in sorted_dict:
-            f.write(curr_chr + '\t' + str(item[0][0]) + '\t' + str(item[0][1]) + '\t' + str(item[1][:-1]) + '\n')
+        i = 0
+        while i < len(sorted_dict):
+            pos_start = sorted_dict[i][0][0]
+            pos_end = sorted_dict[i][0][1]
+            label = sorted_dict[i][1]
+            i += 1
+            if curr_chr == 'chr16' and pos_start > 45060000 and pos_end < 45070000:
+                print 'start: s = ' + str(pos_start) + ', e = ' + str(pos_end) + ', l = ' + label
+                print sorted_dict[i]
+            while i < len(sorted_dict) and sorted_dict[i][0][0] < pos_end:
+                if curr_chr == 'chr16' and pos_start > 45060000 and pos_end < 45070000:
+                    print 'found: ' + str(sorted_dict[i])
+                pos_start = min(sorted_dict[i][0][0], pos_start)
+                pos_end = max(sorted_dict[i][0][1], pos_end)
+                label += sorted_dict[i][1]
+                i += 1
+            if curr_chr == 'chr16' and pos_start > 45060000 and pos_end < 45070000:
+                print 'final: s = ' + str(pos_start) + ', e = ' + str(pos_end) + ', l = ' + clean_label(label)
+            f.write(curr_chr + '\t' + str(pos_start) + '\t' + str(pos_end) + '\t' + clean_label(label) + '\n')
